d5e1b96397d89ab0ac73481b242583441567e078
martssmt <lozanomarcosmarta@gmail.com>
2025-12-07 15:20:30 +0100

    Apartado 2.F --> Consulta Hecha

--- apartado2-sql/consultas-individuales/apartadoF.sql
+++ apartado2-sql/consultas-individuales/apartadoF.sql
@@ -2,3 +2,21 @@
 -- Obtener los doctores (nombre y posicion) que han realizado todos los
 -- procedimientos médicos con coste superior a 5000 y que haya realizado
 -- más de 3 procedimientos médicos de cualquiera de los tipos en total.
+
+SELECT p.name, p.position FROM physician p
+WHERE 
+    -- El doctor ha realizado todos los procedimientos medicos con coste superior a 5000
+    NOT EXISTS (
+            SELECT mp.code FROM medical_procedure mp
+            WHERE mp.cost > 5000
+            AND mp.code NOT IN (
+                    SELECT u.procedureid FROM undergoes u
+                    WHERE u.physicianid = p.employeeid
+                )
+        )
+    -- El médico ha realizado más de 3 procedimientos médicos en total
+    AND p.employeeid IN (
+        SELECT u2.physicianid FROM undergoes u2
+        GROUP BY u2.physicianid
+        HAVING COUNT(*) > 3
+    );
