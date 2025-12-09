41dbed88de092ff35cbb59c095add8307bfa3af1
martssmt <lozanomarcosmarta@gmail.com>
2025-12-09 19:35:36 +0100

    Apartado 3.2 --> Creación de usuarios hecho

--- apartado3-programacion-y-ficheros/apartado2.sql
+++ apartado3-programacion-y-ficheros/apartado2.sql
@@ -1,2 +1,14 @@
 -- Apartado 2
 -- Añade un usuario a la base de datos y dale permiso de lectura sobre la vista anteriormente creada. 
+
+CREATE USER 'usuario_lector' IDENTIFIED BY 'contraseña123';
+
+GRANT SELECT ON hospital_management_system.medication_prescribed TO 'usuario_lector';
+
+-- Consulto sus permisos para verificar que se han aplicado correctamente
+
+SHOW GRANTS FOR 'usuario_lector';
+
+-- Elimino el usuario de prueba para mantener la integridad de los datos
+
+DROP USER 'usuario_lector';
