838e456880c9d951ca0a4a2d9ff6370209c66783
tomas <141403003+tomastte@users.noreply.github.com>
2025-12-13 18:11:33 +0100

    Apartado 2.A --> Consulta hecha

--- apartado2-sql/consultas-individuales/apartadoA.sql
+++ apartado2-sql/consultas-individuales/apartadoA.sql
@@ -3,11 +3,7 @@
 -- crear la base de datos hospital management system y cargar todos los datos disponibles
 -- que van a ser objeto de procesos en puntos posteriores.
 DROP DATABASE IF EXISTS hospital_management_system;
-CREATE DATABASE hospital_management_system
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_spanish_ci;
 
+SOURCE ../preset-data-creation/hospital_tables.sql;
 USE hospital_management_system;
-
-SOURCE ..\preset-data-creation\hospital_tables.sql;
-SOURCE ..\preset-data-creation\hospital_data.sql;
+SOURCE ../preset-data-creation/hospital_data.sql;
